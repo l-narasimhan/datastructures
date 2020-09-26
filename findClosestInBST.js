832b545e2b0538c105b35ca736dcf1e9a04791c6
lnarasimhan83 <l.narasimhan@gmail.com>
2020-09-26 14:31:35 +0530

    Create findClosestInBST.js
    Program to find the closest in a BST, used a brute force. Can be further optimized

---
+++ findClosestInBST.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+       searchClosest(){
+        var data = [];
+        var data_index = [];
+        var current;
+        const targetNum = 12;
+
+        function traverse(node){
+            if(targetNum > node.value){
+                data.push(node.value);
+                if(node.right) traverse(node.right)
+            }else {
+                data.push(node.value);
+                if(node.left) traverse(node.left)
+            }
+        traverse(this.root);
+        var i;
+        for (i=0;i<data.length;i++){
+            if(targetNum >= data[i]){
+                data_index[i] = targetNum - data[i]
+            }else{
+                data_index[i] = data[i] - targetNum;
+            }
+        }
+        var index = data_index.indexOf(Math.min(...data_index));
+        return data[index];
+      }
+    }
