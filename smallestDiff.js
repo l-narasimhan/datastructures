fb8a4d7b38a42acce5b9058ec035b1aa3bf01b7d
lnarasimhan83 <l.narasimhan@gmail.com>
2020-10-03 23:59:08 +0530

    Create smallestDiff.js
    Code to find the smallest difference between two files

---
+++ smallestDiff.js
@@ -0,0 +1,31 @@
+function smallestDifference(arrayOne, arrayTwo) {
+
+      arrayOne.sort((a, b) => a - b);
+      arrayTwo.sort((a,b) => a - b);
+      var arr = [];
+
+      var lowestNum = arrayOne[0] - arrayTwo[0];
+      lowestNum = Math.abs(lowestNum);
+      var tempLowestNum;
+      var i = 0,j = 0;
+
+      while(i <= arrayOne.length && j <= arrayTwo.length){
+
+        tempLowestNum = arrayOne[i] - arrayTwo[j];
+        tempLowestNum = Math.abs(tempLowestNum)
+        if (lowestNum > tempLowestNum){
+
+            lowestNum = tempLowestNum;
+            arr = [arrayOne[i], arrayTwo[j]]
+        }
+        if (arrayOne[i] < arrayTwo[j]){
+            i++;
+        }else{
+            j++;
+        }
+
+       console.log(arr);
+
+      }
+return arr;
+}
